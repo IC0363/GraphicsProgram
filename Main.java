596e048a9ef6301f046b37026b12d46da177acfe
IC0363 <99269263+IC0363@users.noreply.github.com>
2024-01-03 17:12:16 -0500

    hi

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args){
+        Rectangle box = new Rectangle(5, 10, 60, 80);
+        box.draw();
+        Ellipse egg = new Ellipse(100, 100, 40, 60);
+        egg.setColor(Color.YELLOW);
+        egg.fill();    
+
+    }
+    
+}
